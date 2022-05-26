3b56b579045632ca91526bcef6ba07a7e2789a0c
Muhamed Muminovic <muminovic.muhamed01@gmail.com>
2022-05-27 01:02:12 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+function mojasufaraandroid() {
+  window.open(
+    "https://play.google.com/store/apps/details?id=com.sufara.moja",
+    "_blank"
+  );
+}
+function mojasufaraios() {
+  window.open(
+    "https://apps.apple.com/us/app/moja-sufara/id1560726386",
+    "_blank"
+  );
+}
+function mojasufarahuawei() {
+  window.open("https://appgallery.huawei.com/#/app/C104147231", "_blank");
+}
+
+function mojprvinamazandroid() {
+  window.open(
+    "https://play.google.com/store/apps/details?id=com.mojprvinamaz.app",
+    "_blank"
+  );
+}
+function mojprvinamazios() {
+  window.open(
+    "https://apps.apple.com/rs/app/moj-prvi-namaz/id1570021798",
+    "_blank"
+  );
+}
+function mojprvinamazhuawei() {
+  window.open("https://appgallery.huawei.com/#/app/C104387319", "_blank");
+}
+
+function islamskikvizandroid() {
+  window.open(
+    "https://play.google.com/store/apps/details?id=com.pitajucene.kviz",
+    "_blank"
+  );
+}
+function islamskikvizios() {
+  window.open(
+    "https://apps.apple.com/rs/app/islamski-kviz-pitajucene-com/id1542476239",
+    "_blank"
+  );
+}
